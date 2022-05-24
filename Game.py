d5b06927bb54d4664bc904e0cd34afb6cdea1b18
dimgrab <99536541+dimgrab@users.noreply.github.com>
2022-05-24 20:08:33 +0300

    Add files via upload

--- Game.py
+++ Game.py
@@ -3,24 +3,19 @@ import random
 
 def move_wrap(obj, move):
     canvas.move(obj, move[0], move[1])
-    if canvas.coords(player)[1] < 0:
-        player_pos = canvas.coords(player)
-        player_pos[1] = 540
-        canvas.coords(player, player_pos)
-    if canvas.coords(player)[1] > 600:
-        player_pos = canvas.coords(player)
-        player_pos[1] = 30
-        canvas.coords(player, player_pos)
-    if canvas.coords(player)[0] < 0:
-        player_pos = canvas.coords(player)
-        player_pos[0] = 540
-        canvas.coords(player, player_pos)
-    if canvas.coords(player)[0] > 600:
-        player_pos = canvas.coords(player)
-        player_pos[0] = 30
-        canvas.coords(player, player_pos)
+    o_c = canvas.coords(obj)
+    if o_c[1] < 0:
+        o_c[1] = 540
+    if o_c[1] > 540:
+        o_c[1] = 0
+    if o_c[0] < 0:
+        o_c[0] = 540
+    if o_c[0] > 540:
+        o_c[0] = 0
+    canvas.coords(obj, o_c)
 def prepare_and_start():
     canvas.delete("all")
+    label.config(text="Найди выход.")
     global player, exit, fires, enemies
     player_pos = (random.randint(1, N_X - 1) * step, random.randint(1, N_Y - 1) * step)
     exit_pos = (random.randint(1, N_X - 1) * step, random.randint(1, N_Y - 1) * step)
@@ -32,44 +27,51 @@ def prepare_and_start():
         fire_pos = (random.randint(1, N_X - 1) * step, random.randint(1, N_Y - 1) * step)
         fire = canvas.create_image(fire_pos[0], fire_pos[1], image=fire_pic, anchor="nw")
         fires.append(fire)    
-    label.config(text="Найди выход!")
     master.bind("<KeyPress>", key_pressed)
     N_ENEMIES = 4
     enemies = []
     for i in range(N_ENEMIES):
         enemy_pos = (random.randint(0, N_X - 1) * step, random.randint(0, N_Y - 1) * step)
         enemy = canvas.create_image(enemy_pos, image=enemy_pic, anchor='nw')
-        enemies.append((enemy, random.choice([always_right, random_move])))   
+        enemies.append((enemy, random.choice([vsi, always_right, random_move])))
     
-def always_right():
+def always_right(a):
     return (step, 0)
-def random_move():
+def always_left(a):
+    return (-step, 0)
+def random_move(a):
     return random.choice([(step, 0), (-step, 0), (0, step), (0, -step)])
-def do_nothing(x):
-    pass
+def vsi(e_c):
+    p_c = canvas.coords(player)
+    if p_c[0] - e_c[0] < 0:
+        a = (-step,)
+    elif p_c[0] - e_c[0] > 0:
+        a = (step,)
+    else:
+        a = (0,)
+    if p_c[1] - e_c[1] < 0:
+        a = a + (-step,)
+    elif p_c[1] - e_c[1] > 0:
+        a = a + (step,)
+    else:
+        a = a + (0,)
+    return a
+def do_nothing(event):
+    if event.keysym == "p":
+        prepare_and_start()
 
 def check_move():
     if canvas.coords(player) == canvas.coords(exit):
         label.config(text="Победа!")
         master.bind("<KeyPress>", do_nothing)
     for f in fires:
-        if canvas.coords(f)[0] + 30 > canvas.coords(player)[0] > canvas.coords(f)[0] - 30 and canvas.coords(f)[1] + 30 >  canvas.coords(player)[1] > canvas.coords(f)[1] - 30:
+        if canvas.coords(f) == canvas.coords(player):
             label.config(text="Ты проиграл!")
             master.bind("<KeyPress>", do_nothing)
     for e in enemies:
-        a = canvas.coords(e[0])
-        if canvas.coords(player) == a:
+        if canvas.coords(player) == canvas.coords(e[0]):
             label.config(text="Ты проиграл.")
             master.bind("<KeyPress>", do_nothing)
-        if a[0] > 540:
-            a[0] = 0
-        if a[0] < 0:
-            a[0] = 540
-        if a[1] > 540:
-            a[1] = 0
-        if a[1] < 0:
-            a[1] = 540
-        canvas.coords(e[0], a)
 def key_pressed(event):
     if event.keysym == 'Up':
         move_wrap(player, (0, -step))
@@ -79,25 +81,25 @@ def key_pressed(event):
         move_wrap(player, (step, 0))
     if event.keysym == "Left":
         move_wrap(player, (-step, 0))
+    if event.keysym == "g":
+        prepare_and_start()
     for enemy in enemies:
-        direction = enemy[1]() 
-        move_wrap(enemy[0], direction)    
+        direction = enemy[1](canvas.coords(enemy[0])) # вызвать функцию перемещения у "врага"
+        move_wrap(enemy[0], direction) # произвести  перемещение    
     check_move()
-
-master = tkinter.Tk()
 
 step = 60
 N_X = 10
 N_Y = 10
+master = tkinter.Tk()
 player_pic = tkinter.PhotoImage(file="2.gif")
 exit_pic = tkinter.PhotoImage(file="3.gif")
 fire_pic = tkinter.PhotoImage(file="1.gif")
 enemy_pic = tkinter.PhotoImage(file="enemy_pic.gif")
 canvas = tkinter.Canvas(master, bg="white", height=step * N_Y, width=step * N_X)
-restart = tkinter.Button(master, text="Начать заново", command=prepare_and_start)
+canvas.pack()
+restart = tkinter.Button(master, text="Начать играть.", command=prepare_and_start)
 restart.pack()
 label = tkinter.Label(master, text="")
 label.pack()
-canvas.pack()
-master.bind("<KeyPress>", do_nothing)
 master.mainloop()
